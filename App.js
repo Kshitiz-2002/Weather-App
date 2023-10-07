60da8ad14a642e44de607d65e3f631cf5ec41853
Kshitiz-2002 <kshitizpandey2002@gmail.com>
2023-10-07 23:54:52 +0530

    Created UI

--- App.js
+++ App.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import UpcomingWeather from './src/screens/UpcomingWeather';
-import City from './src/screens/City';
-import { NavigationContainer } from '@react-navigation/native'; 
-import CurrentWeather from './src/screens/CurrrentWeather';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './src/components/Tabs';
 
-
-const Tab = createBottomTabNavigator()
- 
 const App = () => {
-  return(
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name={'Cuurent'} component={CurrentWeather}/>
-          <Tab.Screen name={'Upcoming'} component={UpcomingWeather}/>
-          <Tab.Screen name={'City'} component={City}/>
-        </Tab.Navigator>
-      </NavigationContainer>
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
   )
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex:1
-  }
-})
 
 export default App;
---
+++ src/components/Tabs.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import UpcomingWeather from '../screens/UpcomingWeather';
+import City from '../screens/City';
+import CurrentWeather from '../screens/CurrrentWeather';
+import { Feather } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator()
+
+const Tabs = () => {
+    return(
+        <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveBackgroundColor: 'white',
+          tabBarStyle:{
+            backgroundColor:'lightblue'
+          },
+          headerStyle:{
+            backgroundColor:'lightblue'
+          },
+          headerTitleStyle:{
+            fontWeight:'bold',
+            fontSize:25,
+            color:'tomato'
+          }
+        }}
+      >
+        <Tab.Screen name={'Current'} component={CurrentWeather} options={{
+          tabBarIcon: ({ focused }) => <Feather name={'droplet'} size={25} color={focused ? 'tomato' : 'black'} />
+        }} />
+        <Tab.Screen name={'Upcoming'} component={UpcomingWeather} options={{
+          tabBarIcon: ({ focused }) => <Feather name={'clock'} size={25} color={focused ? 'tomato' : 'black'} />
+        }} />
+        <Tab.Screen name={'City'} component={City} options={{
+          tabBarIcon: ({ focused }) => <Feather name={'home'} size={25} color={focused ? 'tomato' : 'black'} />
+        }} />
+      </Tab.Navigator>
+    )
+};
+
+export default Tabs;
